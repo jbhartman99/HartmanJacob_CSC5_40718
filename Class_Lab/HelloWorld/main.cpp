e2f4a6e5ffe1105bf730b4499224d1bfcadc534a
jbhartman99 <jbhartman99@gmail.com>
2015-01-06 20:03:05 -0800

    Signed-off-by: jbhartman99 <jbhartman99@gmail.com>

---
+++ Class_Lab/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/* 
+   File:   main.cpp
+   Author: Jacob Hartman
+   Created on January 6, 2015, 6:05 PM
+   Purpose: To create my first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World!"<<endl;
+    //Exit Stage Right
+    return 0;
+}
+
