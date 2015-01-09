d1cefe8bdd54f50c4a73ef950b4ec28d76d0de94
jbhartman99 <jbhartman99@gmail.com>
2015-01-08 21:55:18 -0800

    Signed-off-by: jbhartman99 <jbhartman99@gmail.com>

---
+++ Hmwk/HelloWorldHomework/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: Jacob Hartman
+ * Created on January 8, 2015, 9:42 PM
+ * Purpose: To Complete the Setup homework problem
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+    //Output Hello World
+    cout<<"Hello World!"<<endl;
+    //Exit Stage Right
+    return 0;
+}
+
+
